93b53ea05c9e2c6ee1634fa75d90421f1a3bdde8
Tim Wade <hello@timjwade.com>
2015-07-30 10:31:57 -0700

    Refactor PullRequestMonitor.
    PullRequestMonitor was an 'iceberg class', making it hard to test. This
    change pulls out the private methods of the monitor into its own class
    and names it PullRequestMonitor. The leftover code was renamed
    PullRequestMonitorWorker and is now a one-method class.

---
+++ app/workers/pull_request_monitor_worker.rb
@@ -0,0 +1,18 @@
+require "pull_request_monitor"
+
+class PullRequestMonitorWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+  include MiqToolsServices::SidekiqWorkerMixin
+  sidekiq_options :queue => :miq_bot, :retry => false
+
+  recurrence { hourly.minute_of_hour(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55) }
+
+  def perform
+    if !first_unique_worker?
+      logger.info "#{self.class} is already running, skipping"
+    else
+      PullRequestMonitor.new.process_repos
+    end
+  end
+end
